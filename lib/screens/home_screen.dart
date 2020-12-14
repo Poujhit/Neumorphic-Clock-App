c45c2015954209798159eaf7501a7e66f18e42d8
Poujhit <poujhit112313@gmail.com>
2020-12-14 22:41:36 +0530

    Added neumorphic bottom nav bar 🐱‍🏍

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,51 +1,70 @@
-import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 import 'package:neumorphic_clock_app/screens/clock_screen.dart';
-import 'package:neumorphic_clock_app/widgets/alarm_tile.dart';
+import 'package:neumorphic_clock_app/screens/stop_watch_screen.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class HomeScreen extends StatelessWidget {
+  final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreens() {
+    return [ClockScreen(), StopWatchScreen()];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.alarm, color: Colors.black54),
+        title: ("Alarm Clock"),
+        activeColor: Color(0xFF707070),
+        inactiveColor: Color(0xFFb9abab),
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.watch, color: Colors.black45),
+        title: ("StopWatch"),
+        activeColor: Color(0xFF707070),
+        inactiveColor: Color(0xFF707070),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      navBarHeight: 80,
       backgroundColor: Color(0xFFb9abab),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).padding.top + 25,
-            ),
-            Container(
-              margin: const EdgeInsets.only(
-                left: 29,
-              ),
-              child: BorderedText(
-                strokeWidth: 1.0,
-                strokeColor: Color(0xFF707070),
-                child: Text(
-                  'Clock',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Color(0xFF806a6a),
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-            ),
-            ClockScreen(),
-            SizedBox(
-              height: 20,
-            ),
-            AlarmTile(0),
-            AlarmTile(1),
-            AlarmTile(2),
-          ],
-        ),
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+
+      stateManagement: true,
+      neumorphicProperties: NeumorphicProperties(
+        bevel: 15,
+        curveType: CurveType.concave,
       ),
+      hideNavigationBarWhenKeyboardShows: true,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Color(0xFFb9abab),
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      padding: NavBarPadding.only(bottom: 15),
+
+      itemAnimationProperties: ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle: NavBarStyle.neumorphic, // Choose the nav bar style with this property.
     );
   }
 }
