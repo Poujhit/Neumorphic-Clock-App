04f462004203833c137a8c5691e59783f05b3c3d
Poujhit <poujhit.dev@gmail.com>
2021-01-12 16:59:16 +0530

    feat: added timer screen 🕗

---
+++ lib/screens/timer_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatefulWidget {
+  @override
+  _TimerScreenState createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
